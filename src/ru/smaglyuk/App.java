da3ec7ad6aff4c4e3fbe5c693e8417640f913ffe
Alex <smaglyuk.alex@gmail.com>
2024-04-08 11:58:20 +0300

    Some changes

--- src/ru/smaglyuk/App.java
+++ src/ru/smaglyuk/App.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class App 
 {
 
-    public int someNumber = 23;
-    
+    public static int someNumber = 23;
+
     public static void main( String[] args )
     {
         Scanner scan = new Scanner(System.in);
         System.out.print("Введите число: ");
         int x = scan.nextInt();
         int y = 2;
-        int result = x*y;
+        int result = x*y+someNumber;
         System.out.println("Ваше число умноженное на 2: " + result);
         scan.close();
     }
