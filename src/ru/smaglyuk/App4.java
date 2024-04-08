e7a71a4faedafa96aeebcab27a60a0e660660e0d
Alex <112931512+Alexey723@users.noreply.github.com>
2024-04-08 11:06:08 +0300

    Update App4.java

--- src/ru/smaglyuk/App4.java
+++ src/ru/smaglyuk/App4.java
@@ -7,7 +7,7 @@ public class App4
     private String name = "Alex";
     private int age = 38;
 
-    public void App2(String name, int age){
+    public void App4(String name, int age){
         this.name = name;
         this.age = age;
     }
