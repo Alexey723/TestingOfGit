a65beda8df499ac228cb501ee7ae515656f21eb7
Alex <smaglyuk.alex@gmail.com>
2024-04-08 10:58:10 +0300

    Modify App4.java

--- src/ru/smaglyuk/App4.java
+++ src/ru/smaglyuk/App4.java
@@ -1,1 +1,15 @@
 
+package ru.smaglyuk;
+
+
+public class App4 
+{
+    private String name = "Alex";
+    private int age = 38;
+
+    public void App2(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+}
