92f65287eef7b80ddff750ab150cf47df916061a
Alex <smaglyuk.alex@gmail.com>
2024-04-08 14:35:28 +0300

    One more commit

--- src/ru/smaglyuk/App.java
+++ src/ru/smaglyuk/App.java
@@ -6,6 +6,7 @@ public class App
 {
 
     public static int someNumber = 23; //добавляем нечто новое, чтобы прошёл ещё один коммит
+    public static int someNumber2 = 23;
 
     public static void main( String[] args )
     {
@@ -13,7 +14,7 @@ public class App
         System.out.print("Введите число: "); //вводим число из консоли
         int x = scan.nextInt();
         int y = 2;  //инициализируем переменную
-        int result = x*y+someNumber;  // проводим арифметические действия
+        int result = x*y+(someNumber-someNumber2);  // проводим арифметические действия
         System.out.println("Ваше число умноженное на 2: " + result);
         scan.close(); //закрываем наш сканнер
     }
